45f6bd3aff2a7b8a23ff3873464900d9d45d14f3
Josefina Nord <jnd20004@student.mdh.se>
2025-03-07 14:29:45 +0100

    Added functionality for merge_sort function

--- merge_queue_begin.py
+++ merge_queue_begin.py
@@ -52,9 +52,33 @@ def merge_sort(S):
     The sorted result is returned in a new queue, S is left empty
     Queues are deques used with the front to the left. """
     level_queues = deque()
-    # TODO: Create a queue for each input element and add them to the level_queues
-    # TODO: while we have more than one queue remaining, merge a level 
-    # TODO: dequeue and return the single remaining merged queue
+    # ------------------------
+    # ADDED: 2025-03-07, Josefina
+    # Step 1: Create a queue for each input element and add them to the level_queues
+    # ------------------------
+    for item in S:
+        level_queues.append(deque([item]))
+
+    # ------------------------
+    # Step 2: While we have more than one queue remaining, merge a level
+    # ------------------------
+    while len(level_queues) > 1:
+        next_level_queues = deque()
+
+        while len(level_queues) > 1:
+            q1 = level_queues.popleft()
+            q2 = level_queues.popleft()
+            next_level_queues.append(merge(q1, q2))
+        
+        if level_queues:
+            next_level_queues.append(level_queues.popleft())
+        
+        level_queues = next_level_queues
+
+    # ------------------------
+    # Step 3: Dequeue and return the single remaining merged queue
+    # ------------------------
+    return level_queues.popleft()
 
 
 if __name__ == '__main__':
